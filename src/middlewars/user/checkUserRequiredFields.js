177d8de5a6401203a51d2b409b63c64d15a3a96f
Abdelmadjid Cherfaoui <madjidou1990@gmail.com>
2019-10-01 14:38:00 +0100

    verification middleware

--- src/middlewars/user/checkUserRequiredFields.js
+++ src/middlewars/user/checkUserRequiredFields.js
@@ -1,11 +1,40 @@
 
+const { check, validationResult } = require('express-validator');
 module.exports = (req, res, next) => {
-    try {
+    const phone  = check(req.body.phone).isMobilePhone();
+    const email = check(req.body.email).isEmail();
 
+    const existedPhone = phone;
+    const existedEmail = email;
+    try {
+        const errors = validationResult(req); 
+        if (!errors.isEmpty()) {
+            res.status(422).json({ errors: errors.array() });
+        return;
+        }
+        if (!phone){
+            res.status(422).json({errors: errors.array()});
+        return;
+        }
+        if (!email){
+            res.status(422).json({errors: errors.array()});
+            return;
+        }
+        if (existedEmail){
+            res.status(400).message("email already existed");
+        return;
+        }
+        if (existedPhone){
+            res.status(400).message("phone already existed");
+        return;
+        }
         next();
+        
     } catch (error) {
         return res.status(501).json({
             message: 'failed to add new user'
         });
     }
+
+
 };
